a53636fb35c92e259142344c89e782ebf41bf52c
Jesko-typeR <jeskotyper@gmail.com>
2026-09-15 23:32:37 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #Sort s and t, since the letter's frequency is same everywhere therefore they would be the same string.
+
+        s = "".join([chr(j) for j in sorted([ord(i) for i in s])])
+        t = "".join([chr(j) for j in sorted([ord(i) for i in t])])
+        return s==t
+
+
+        
+        
